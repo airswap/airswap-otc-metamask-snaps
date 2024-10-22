c9bca193f145a049a8483b526e80121c7da6384f
Mike Starr <imhs@protonmail.com>
2024-10-22 16:13:21 -0400

    changed port to 8000

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
+import {
+  Card,
+  ConnectButton,
+  InstallFlaskButton,
+  ReconnectButton,
+  SendHelloButton,
+} from '../components';
+import { defaultSnapOrigin } from '../config';
+import {
+  useMetaMask,
+  useInvokeSnap,
+  useMetaMaskContext,
+  useRequestSnap,
+} from '../hooks';
+import { isLocalSnap, shouldDisplayReconnectButton } from '../utils';
 import { SwapWidget } from './SwapWidget/SwapWidget';
-
-// import { defaultSnapOrigin } from '../config';
-// import {
-//   useMetaMask,
-//   useInvokeSnap,
-//   useMetaMaskContext,
-//   useRequestSnap,
-// } from '../hooks';
-// import { isLocalSnap, shouldDisplayReconnectButton } from '../utils';
 
 const Container = styled.div`
   display: flex;
@@ -34,32 +40,30 @@ const Heading = styled.h1`
 `;
 
 const Index = () => {
-  // const { error } = useMetaMaskContext();
-  // const { isFlask, snapsDetected, installedSnap } = useMetaMask();
-  // const requestSnap = useRequestSnap();
-  // const invokeSnap = useInvokeSnap();
+  const { error } = useMetaMaskContext();
+  const { isFlask, snapsDetected, installedSnap } = useMetaMask();
+  const requestSnap = useRequestSnap();
+  const invokeSnap = useInvokeSnap();
 
-  // const isMetaMaskReady = isLocalSnap(defaultSnapOrigin)
-  //   ? isFlask
-  //   : snapsDetected;
+  const isMetaMaskReady = isLocalSnap(defaultSnapOrigin)
+    ? isFlask
+    : snapsDetected;
 
-  // const handleSendHelloClick = async () => {
-  //   await invokeSnap({ method: 'hello' });
-  // };
+  const handleSendHelloClick = async () => {
+    await invokeSnap({ method: 'hello' });
+  };
 
   return (
     <Container>
       <Heading>AirSwap OTC</Heading>
       <SwapWidget />
-      {/* <Subtitle>
-        Get started by editing <code>src/index.tsx</code>
-      </Subtitle> */}
 
-      {/* <CardContainer>
+      {/* delete */}
+      <div>
         {error && (
-          <ErrorMessage>
+          <div>
             <b>An error happened:</b> {error.message}
-          </ErrorMessage>
+          </div>
         )}
         {!isMetaMaskReady && (
           <Card
@@ -123,15 +127,16 @@ const Index = () => {
             !shouldDisplayReconnectButton(installedSnap)
           }
         />
-        <Notice>
+        <div>
           <p>
             Please note that the <b>snap.manifest.json</b> and{' '}
             <b>package.json</b> must be located in the server root directory and
             the bundle must be hosted at the location specified by the location
             field.
           </p>
-        </Notice>
-      </CardContainer> */}
+        </div>
+      </div>
+      {/* delete */}
     </Container>
   );
 };
